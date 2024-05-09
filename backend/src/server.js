3b9f07cb28b1027fe344e60a95420c11342527fa
Awab Elsadig <elsadigawab@gmail.com>
2024-05-09 23:34:01 +0300

    update 6 files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,32 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import cors from "cors";
-import foodRouter from "./routers/food.router.js";
-import userRouter from "./routers/user.router.js";
-import restaurantRouter from "./routers/restaurant.router.js";
+import express from 'express';
+import cors from 'cors';
+import foodRouter from './routers/food.router.js';
+import userRouter from './routers/user.router.js';
+import restaurantRouter from './routers/restaurant.router.js';
 
-import { dbconnect } from "./config/database.config.js";
+import { dbconnect } from './config/database.config.js';
 dbconnect();
 
 const app = express();
 
 app.use(express.json());
 
-app.use(
-	cors({
-		credentials: true,
-		origin: ["http://localhost:1234"],
-	})
-);
+// app.use(
+// 	cors({
+// 		credentials: true,
+// 		origin: ["http://localhost:1234"],
+// 	})
+// );
 
-app.use("/api/foods", foodRouter);
-app.use("/api/users", userRouter);
-app.use("/api/restaurants", restaurantRouter);
+app.use('/api/foods', foodRouter);
+app.use('/api/users', userRouter);
+app.use('/api/restaurants', restaurantRouter);
 
 const PORT = 5678;
 
 app.listen(PORT, () =>
-	console.log("Listening on port http://localhost:" + PORT)
+	console.log('Listening on port http://localhost:' + PORT)
 );
